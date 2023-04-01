7ca76314a7fbfdb3ed857487448b7f3f47b2b49f
Mohammad Asif Siddique <mdasifsidd@gmail.com>
2023-04-01 13:05:19 +0530

    initial commit

---
+++ src/components/ProductList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useFilterContext } from '../context/filter_context'
+import GridView from './GridView'
+import ListView from './ListView'
+
+const ProductList = () => {
+  const {filtered_products:products , gridView} = useFilterContext()
+  if(products.length < 1){
+  return  <h5 className={{textTransform : 'none'}}>
+      sorry ,no products matched your search...
+    </h5>
+  }
+  if(gridView === false){
+    return <ListView products = {products}/>
+  }
+  return <GridView products ={products}>product list</GridView>
+}
+
+export default ProductList
